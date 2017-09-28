f8ea0a2a8b40633bbf8c1fc7b93369bb5774517e
Matthew Kloecker <matthew.kloecker@gmail.com>
2017-09-28 13:21:39 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,4 +1,4 @@
 # Write your #display_rainbow method here
-def display_rainbow(color_1, color_2, color_3, color_4, color_5, color_6)
+def display_rainbow(color_1, color_2, color_3, color_4, color_5, color_6, color_7)
 
 end
